033a8a59c70ba6157a573b50cf0827a48260675b
Bhavya Singh <bhavya.cse.22@nitap.ac.in>
2025-04-28 07:25:02 +0530

    Time: 2 ms (100%), Space: 61.5 MB (61.14%) - LeetHub

---
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long countSubarrays(int[] nums, long k) {
+        long res = 0, total = 0;
+        for(int i = 0, j=0;j<nums.length;j++){
+            total += nums[j];
+            while(i<=j && total*(j-i+1) >= k){
+                total -= nums[i];
+                i++;
+            }
+            res += (j-i+1);
+        }
+        return res;
+    }
+}
