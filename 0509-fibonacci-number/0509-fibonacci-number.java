744eb32e7934ada3525ce16c1cfdcb3e108973fe
Bhavya Singh <bhavya.cse.22@nitap.ac.in>
2025-08-22 07:21:41 +0530

    Time: 0 ms (100%), Space: 40.7 MB (27.29%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int fibonacci(int n,int[] dp){
+        if(n<=1){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int a = fibonacci(n-1,dp);
+        int b = fibonacci(n-2,dp);
+        dp[n] = a+b;
+        return dp[n];
+    }
+    public int fib(int n) {
+        int[] dp = new int[n+1];
+        for(int i=0;i<n+1;i++){
+            dp[i] = -1;
+        }
+        return fibonacci(n,dp);
+    }
+}
